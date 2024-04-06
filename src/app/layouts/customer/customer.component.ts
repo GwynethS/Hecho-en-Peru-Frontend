52d56795a80e308aae1f12703c8a18803fc0f286
Gwyneth Lyu Gisele Segura Silva <gwynethsegura@gmail.com>
2024-04-06 16:00:27 -0500

    Main modules and components were created
    The admin, customer and not-found module and component were created. The shared module was also created.

---
+++ src/app/layouts/customer/customer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer',
+  templateUrl: './customer.component.html',
+  styleUrl: './customer.component.scss'
+})
+export class CustomerComponent {
+
+}
