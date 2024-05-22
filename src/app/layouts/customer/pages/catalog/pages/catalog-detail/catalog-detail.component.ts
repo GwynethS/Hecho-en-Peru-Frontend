7e24926f0bd7cd3a5a342a702873c56a05e2c13c
GwynethS <gwynethsegura@gmail.com>
2024-05-22 00:49:47 -0500

    feat: se agregó el paginador a la sección de comentarios

--- src/app/layouts/customer/pages/catalog/pages/catalog-detail/catalog-detail.component.ts
+++ src/app/layouts/customer/pages/catalog/pages/catalog-detail/catalog-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-catalog-detail',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './catalog-detail.component.scss'
 })
 export class CatalogDetailComponent {
+  length = 50;
+  pageSize = 12;
+  pageIndex = 0;
 
+  pageEvent!: PageEvent;
+
+  handlePageEvent(e: PageEvent) {
+    this.pageEvent = e;
+    this.length = e.length;
+    this.pageSize = e.pageSize;
+    this.pageIndex = e.pageIndex;
+  }
 }
