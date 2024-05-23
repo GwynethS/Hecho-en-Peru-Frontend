0017e09106a95480d676f5a151d49c27171a8481
GwynethS <gwynethsegura@gmail.com>
2024-05-22 21:43:33 -0500

    feat: se configuró el carrusel de productos más vendidos en el componente home

--- src/app/layouts/customer/pages/home/home.component.ts
+++ src/app/layouts/customer/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,8 +8,25 @@ import { Router } from '@angular/router';
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  length = 5;
+  pageSize = 3;
+  pageIndex = 0;
 
   constructor(private router: Router) {}
+
+  previousPage(){
+    if(this.pageIndex > 0){
+      this.pageIndex--;
+      console.log(this.pageIndex);
+    }
+  }
+
+  nextPage(){
+    if(this.pageIndex < this.length - 1){
+      this.pageIndex++;
+      console.log(this.pageIndex);
+    }
+  }
 
   redirectTo(page: string): void {
     this.router.navigate([`/shop/${page}`]);
