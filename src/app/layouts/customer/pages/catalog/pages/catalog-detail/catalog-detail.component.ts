2b93b5abc2b24619376c234ed3fa8c2f93ea42eb
GwynethS <gwynethsegura@gmail.com>
2024-05-25 00:50:45 -0500

    feat: se enlazó el componente catalog-detail con el backend y se realizó la paginación del catálogo de productos

--- src/app/layouts/customer/pages/catalog/pages/catalog-detail/catalog-detail.component.ts
+++ src/app/layouts/customer/pages/catalog/pages/catalog-detail/catalog-detail.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PageEvent } from '@angular/material/paginator';
+import { ProductsService } from '../../../../../admin/pages/products/products.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Product } from '../../../../../admin/pages/products/models/product';
 
 @Component({
   selector: 'app-catalog-detail',
@@ -18,17 +22,42 @@ export class CatalogDetailComponent {
 
   avgRating = 4.5;
 
-  constructor(private fb: FormBuilder) {
+  productSelected: Product | null = null;
+
+  subscriptions: Subscription[] = [];
+
+  constructor(
+    private fb: FormBuilder,
+    private productsService: ProductsService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.subscriptions.push(
+      this.productsService
+        .getSearchProductDetailsByID(this.route.snapshot.params['id'])
+        .subscribe({
+          next: (findedProduct) => {
+            if (findedProduct) {
+              this.productSelected = findedProduct;
+              this.avgRating = this.productSelected.averageRating;
+            }
+          },
+          error: () => {
+            this.router.navigate([`/404`]);
+          }
+        })
+    );
+
     this.commentForm = this.fb.group({
       textCommentary: this.fb.control('', Validators.required),
       rating: this.fb.control('', Validators.required),
     });
   }
 
-  onSubmit(){
-    if(this.commentForm.invalid){
+  onSubmit() {
+    if (this.commentForm.invalid) {
       this.commentForm.markAllAsTouched();
-    }else{
+    } else {
       console.log(this.commentForm.value);
     }
   }
