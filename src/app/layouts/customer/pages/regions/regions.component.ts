1b2562a53119a472d5e1c6ca33483c290c2e36d2
Gonzalo Pingo Amaya <pingoamayagonzalo@gmail.com>
2024-05-26 00:44:13 -0500

    feat: se implementó la visualización de las cards de regiones dinamicamente

--- src/app/layouts/customer/pages/regions/regions.component.ts
+++ src/app/layouts/customer/pages/regions/regions.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Region } from '../../../admin/pages/regions/models/region';
+import { Subscription } from 'rxjs';
+import { RegionsService } from '../../../admin/pages/regions/regions.service';
 
 @Component({
   selector: 'app-regions',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrl: './regions.component.scss'
 })
 export class RegionsComponent {
+  regions: Region[] = [];
+  subscription: Subscription[] = [];
 
+  constructor(
+    private regionsService: RegionsService
+  ) {}
+
+  ngOnInit(): void {
+    this.subscription.push(
+      this.regionsService.getRegions().subscribe({
+        next: (regions) => {
+          this.regions = regions;
+        }
+      })
+    );
+  }
 }
