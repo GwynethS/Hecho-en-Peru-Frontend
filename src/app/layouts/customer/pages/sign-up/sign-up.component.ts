304ec59b267bb785d22ea427f48684b0ae61561d
GwynethS <102232553+GwynethS@users.noreply.github.com>
2024-05-28 07:48:07 -0500

    feat: se implementó el registro de usuario

--- src/app/layouts/customer/pages/sign-up/sign-up.component.ts
+++ src/app/layouts/customer/pages/sign-up/sign-up.component.ts
@@ -1,37 +1,51 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CustomersService } from '../../../admin/pages/customers/customers.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.scss'
+  styleUrl: './sign-up.component.scss',
 })
 export class SignUpComponent {
   signUpForm: FormGroup;
   revealPassword = false;
 
-  constructor(private fb: FormBuilder){
+  subscriptions: Subscription[] = [];
+
+  constructor(
+    private fb: FormBuilder,
+    private customersService: CustomersService
+  ) {
     this.signUpForm = this.fb.group({
       name: this.fb.control('', [
         Validators.required,
         Validators.minLength(2),
-        Validators.pattern('[a-zA-Z\\s]*')
+        Validators.pattern('[a-zA-Z\\s]*'),
       ]),
       lastName: this.fb.control('', [
         Validators.required,
         Validators.minLength(2),
-        Validators.pattern('[a-zA-Z\\s]*')
+        Validators.pattern('[a-zA-Z\\s]*'),
       ]),
       email: this.fb.control('', [Validators.required, Validators.email]),
-      password: this.fb.control('', Validators.required)
-    })
+      password: this.fb.control('', [Validators.required, Validators.min(8)]),
+    });
   }
 
-  OnSubmit(): void{
-    if(this.signUpForm.invalid){
+  OnSubmit(): void {
+    if (this.signUpForm.invalid) {
       this.signUpForm.markAllAsTouched();
-    }else{
+    } else {
       console.log(this.signUpForm.value);
+      this.subscriptions.push(
+        this.customersService.createUser(this.signUpForm.value).subscribe({
+          next: (user) => {
+            console.log(user);
+          },
+        })
+      );
     }
   }
 }
