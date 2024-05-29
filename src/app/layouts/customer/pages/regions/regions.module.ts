ef0af91dfabd3b950827ff5d1a66a4aeb7be7e9a
Gonzalo Pingo Amaya <pingoamayagonzalo@gmail.com>
2024-05-29 02:00:10 -0500

    feat: se implementó la estructura html y los estilos del detalle de las regiones

--- src/app/layouts/customer/pages/regions/regions.module.ts
+++ src/app/layouts/customer/pages/regions/regions.module.ts
@@ -6,11 +6,13 @@ import { RegionsComponent } from './regions.component';
 import { SharedModule } from 'primeng/api';
 import { MaterialModule } from '../../../../shared/material/material.module';
 import { RegionsService } from '../../../admin/pages/regions/regions.service';
+import { RegionDetailComponent } from './pages/region-detail/region-detail.component';
 
 
 @NgModule({
   declarations: [
-    RegionsComponent
+    RegionsComponent,
+    RegionDetailComponent
   ],
   imports: [
     CommonModule,
