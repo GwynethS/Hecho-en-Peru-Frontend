b910e28a5e4a344433aa47558e8a6412d88313a0
Gonzalo Pingo Amaya <pingoamayagonzalo@gmail.com>
2024-05-29 02:00:10 -0500

    feat: se implementó la estructura html y los estilos del detalle de las regiones

---
+++ src/app/layouts/customer/pages/regions/pages/region-detail/region-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-region-detail',
+  templateUrl: './region-detail.component.html',
+  styleUrl: './region-detail.component.scss'
+})
+export class RegionDetailComponent {
+
+}
--- src/app/layouts/customer/pages/regions/regions-routing.module.ts
+++ src/app/layouts/customer/pages/regions/regions-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegionsComponent } from './regions.component';
+import { RegionDetailComponent } from './pages/region-detail/region-detail.component';
 
 const routes: Routes = [
   {
     path: '',
     component: RegionsComponent
+  },
+  {
+    path: 'hola',
+    component: RegionDetailComponent
+  
   }
 ];
 
